fa7766c08009e1a9df191587a42cf53be680fea2
Utilisateur <frank.owen.brunet@gmail.com>
2021-01-04 11:07:34 +0100

    verif admin create subject category

--- Back-end/api/src/routes/routes.js
+++ Back-end/api/src/routes/routes.js
@@ -10,6 +10,7 @@ const verif_token = require("../middleware/token");
 
 // ADD AN USER SIGNUP
 router.post("/user/sign-up", (req, res) => {
+    let admin = 0;
     let verif = `SELECT email FROM utilisateurs WHERE email = '${req.body.email}';`;
     con.query(verif, (err, result) => {
         if (err) throw err;
@@ -17,7 +18,7 @@ router.post("/user/sign-up", (req, res) => {
             res.status(200).send("This email is already in use");
         } else {
             bcrypt.hash(req.body.password, saltRounds).then((hashPassword) => {
-                let sql = `INSERT INTO utilisateurs (email, prenom, pseudo, password, avatar) VALUES('${req.body.email}','${req.body.prenom}','${req.body.pseudo}','${hashPassword}','${req.body.avatar}');`;
+                let sql = `INSERT INTO utilisateurs (email, prenom, pseudo, password, avatar, administ) VALUES('${req.body.email}','${req.body.prenom}','${req.body.pseudo}','${hashPassword}','${req.body.avatar}','${admin}');`;
                 con.query(sql, (err, result) => {
                     if (err) throw err;
                     res.status(200).send("users well inserted");
@@ -59,13 +60,13 @@ router.post("/user/login", (req,res) => {
 });
 
 // EDIT PROFIL USER
-router.put("/user/edit", (req, res) => {
+router.put("/user/edit/:id", (req, res) => {
     let prenom = req.body.prenom;
     let email = req.body.email;
     let password = req.body.password;
     let avatar = req.body.avatar;
     let pseudo = req.body.pseudo;
-    let id = req.body.id;
+    let id = req.params.id;
   
     let check = `SELECT id FROM utilisateurs WHERE id = '${id}';`;
     con.query(check, (err, result) => {
@@ -118,13 +119,22 @@ router.post("/subject/create/:idUser&:idCategorySubject", (req,res) => {
 //CREATE CATEGORY SUBJECT
 router.post("/subject/category/:idAdmin", (req,res) => {
 
-    let idAdmin = req.params.id;
-    let sql = `INSERT INTO catégories_sujet (nom) VALUES('${req.body.nom}')`;
+    let idAdmin = req.params.idAdmin;
+    let checkAdmin = `SELECT id,administ FROM utilisateurs WHERE id = '${idAdmin}';`;
+    con.query(checkAdmin, (err,result) => {
+      if (err) throw err;
+      else if (result.administ == 1){
+        let sql = `INSERT INTO catégories_sujet (nom) VALUES('${req.body.nom}')`;
 
-    con.query(sql, (err, result) => {
-        if(err) throw err;
-        res.status(200).send("New category added")
+        con.query(sql, (err, results) => {
+            if(err) throw err;
+            res.status(200).send("New category added")
+        })
+      } else{
+        res.status(200).send("Ur not an admin user")
+      }
     })
+   
 });
 module.exports = router;
 
