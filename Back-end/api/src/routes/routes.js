15ea858fab7788255a4b197130eba89104ead504
Utilisateur <frank.owen.brunet@gmail.com>
2021-01-04 17:13:58 +0100

    routes delete commentary

--- Back-end/api/src/routes/routes.js
+++ Back-end/api/src/routes/routes.js
@@ -68,7 +68,7 @@ router.post("/user/login", (req,res) => {
 });
 
 // EDIT PROFIL USER
-router.put("/user/edit/:id", (req, res) => {
+router.put("/user/edit/:id",verif_token, (req, res) => {
   try {
     let prenom = req.body.prenom;
     let email = req.body.email;
@@ -102,7 +102,7 @@ router.put("/user/edit/:id", (req, res) => {
 });
 
 // DELETE USER ACCOUNT
-router.delete("/user/:id", (req,res) => {
+router.delete("/user/:id",verif_token, (req,res) => {
   try {
     let id = req.params.id;
 
@@ -122,8 +122,8 @@ router.delete("/user/:id", (req,res) => {
 // CREATE SUBJECT
 router.post("/subject/create",verif_token, (req,res) => {
   try {
-    let id = req.body.idUser;
-    let contained = req.body.contenu;
+    let id = req.body.id;
+    let contained = req.body.contained;
     let idSubject = req.body.idCategorySubject;
 
     let sql = `INSERT INTO sujet_forum (id_utilisateur, date, contenu, id_catégories_sujet) VALUES('${id}',NOW(),'${contained}','${idSubject}');`;
@@ -140,8 +140,8 @@ router.post("/subject/category",verif_token, (req,res) => {
   try {
     if(!req.body.idAdmin) throw "please provide idAdmin"
     if(!req.body.nom || req.body.nom == "") throw "please provide a nom"
-    let idAdmin = req.body.idAdmin;
-    let checkAdmin = `SELECT id,administ FROM utilisateurs WHERE id = '${idAdmin}';`;
+    let id = req.body.idAdmin;
+    let checkAdmin = `SELECT id,administ FROM utilisateurs WHERE id = '${id}';`;
     con.query(checkAdmin, (err,result) => {
       if (err) throw err;
       else if(result[0].administ == 1){
@@ -159,5 +159,46 @@ router.post("/subject/category",verif_token, (req,res) => {
       res.status(203).send(error)
   }  
 });
+
+// CREATE A COMMENTARY
+router.post("/subject/commentaries", (req,res) => {
+  try {
+    let idAuthor = req.body.idAuthor;
+    let contained = req.body.contained;
+    let idSubject = req.body.idSubject;
+    // let idCategory = req.body.idCategory;
+
+    let sql = `INSERT INTO commentaires (id_auteur, date_commentaires, contenu_commentaires, id_sujet_forum) VALUES('${idAuthor}',NOW(),'${contained}','${idSubject}')`;
+    con.query(sql, (err,result) => {
+      if (err) throw err;
+      res.status(200).send("New commentary added")
+    })
+
+  } catch (error) {
+      res.status(203).send(error)
+  }
+});
+
+// DELETE A COMMENTARY
+router.delete("/subject/:id/:idCommentary", (req,res) => {
+  try {
+    let id = req.params.id;
+    let idCommentary = req.params.idCommentary;
+    let sql = `DELETE FROM commentaires WHERE commentaires.id_auteur = '${id}' AND id = '${idCommentary}'`;
+
+    con.query(sql, (err,result) => {
+      if (err) throw err;
+      if (result.length){
+        res.status(200).send("USER COMMENTARY DELETED");
+      } else {
+        res.status(200).send("THIS COMMENTARY DOES NOT EXIST")
+      }
+            
+    });    
+  } catch (error) {
+    res.status(203).send(error)
+  }
+});
+
 module.exports = router;
 
