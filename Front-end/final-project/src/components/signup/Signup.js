ea64ebfbc31d891e328933d6bdbb54063a9ea112
Owranges <frank.owen.brunet@gmail.com>
2021-01-10 15:33:38 +0100

    Signup form

--- Front-end/final-project/src/components/signup/Signup.js
+++ Front-end/final-project/src/components/signup/Signup.js
@@ -25,6 +25,18 @@ function Signup() {
                         <label>Mot de passe:</label>
                         <input type="password" name="password" id="password" required onChange={e => setPassword(e.target.value)} />
                     </div>
+                    <div className="form-pseudo">
+                        <label>Mot de passe:</label>
+                        <input type="pseudo" name="pseudo" id="pseudo" required onChange={e => setPseudo(e.target.value)} />
+                    </div>
+                    <div className="form-firstname">
+                        <label>Mot de passe:</label>
+                        <input type="firstname" name="firstname" id="firstname" required onChange={e => setFirstname(e.target.value)} />
+                    </div>
+                    <div className="form-avatar">
+                        <label>Mot de passe:</label>
+                        <input type="avatar" name="avatar" id="avatar" required onChange={e => setAvatar(e.target.value)} />
+                    </div>
                 </form>
                 <div className="form-btn">
                     <button className="btnGreen">CONNEXION</button>
