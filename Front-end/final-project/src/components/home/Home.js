7a4acec3414c3767c0639b0459ee417d9d2386ac
Owranges <frank.owen.brunet@gmail.com>
2021-01-11 10:02:05 +0100

    change css Signin/up + EditProfile page début

--- Front-end/final-project/src/components/home/Home.js
+++ Front-end/final-project/src/components/home/Home.js
@@ -8,10 +8,12 @@ import ff7midgar from "../../Img/HomeImg/ff7midgar.jpg"
 
 function Home() {
     return (
-        <div>
+        <div className="homes">
             <Header />
             <div className="homeDiv" >
-                <p className="homeMsg" >FINAL FANTASY VII REMAKE <br />FINAL FANTASY FORUM GUIDE <br /> Bienvenue à toi ! <br />Rejoins toi aussi notre forum en t’inscrivant pour profiter des actualités sur le jeu ainsi que de découvrir les subtilités de cet univers avec d’autres fans comme toi.</p>
+                <div className="homeMsgDiv">
+                    <p className="homeMsg" >FINAL FANTASY VII REMAKE <br />FINAL FANTASY FORUM GUIDE <br /> Bienvenue à toi ! <br />Rejoins toi aussi notre forum en t’inscrivant pour profiter des actualités sur le jeu ainsi que de découvrir les subtilités de cet univers avec d’autres fans comme toi.</p>
+                </div>
                 <img src={ff7midgar} className='midgarImg' alt='Logo meteor from ff7' />
                 <img src={ff7cloudprofile} className='cloudImg' alt='Logo meteor from ff7' />
             </div>
